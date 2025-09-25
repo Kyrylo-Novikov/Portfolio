adc34762ae49bc522bf2c3c44b5bf9183beb182a
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-09-25 09:13:39 +0200

    feat(header): add styled header and integrate it into landing component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HeaderComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
