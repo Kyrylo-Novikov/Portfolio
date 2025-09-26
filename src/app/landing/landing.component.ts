ba334c03653779ec552bb1c83e9f9134db0e4c4b
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-09-26 08:05:25 +0200

    feat(about-me): add About Me component and related images

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { HeaderComponent } from '../shared/header/header.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent],
+  imports: [HeroComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
