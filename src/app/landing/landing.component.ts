7cef06d5e91c024a1d8f4cab4f2c47afb4308d5f
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-10-04 12:42:16 +0200

    feat(my-skills): complete disign of skills section with placeholder text

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
+import { MySkillsComponent } from '../my-skills/my-skills.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent, AboutMeComponent],
+  imports: [
+    HeroComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    ProjectsComponent,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
-
+  techsArr: string[] = [
+    'assets/imgs/tech-icon/current-tech/HTML.svg',
+    'assets/imgs/tech-icon/current-tech/CSS.svg',
+    'assets/imgs/tech-icon/current-tech/JavaScript.svg',
+    'assets/imgs/tech-icon/current-tech/Material Design.svg',
+    'assets/imgs/tech-icon/current-tech/TypeScript.svg',
+    'assets/imgs/tech-icon/current-tech/Angular.svg',
+    'assets/imgs/tech-icon/current-tech/Firebase.svg',
+    'assets/imgs/tech-icon/current-tech/Git.svg',
+    'assets/imgs/tech-icon/current-tech/Rest-Api.svg',
+    'assets/imgs/tech-icon/current-tech/Scrum.svg',
+    'assets/imgs/tech-icon/current-tech/GrowthMindset.svg',
+  ];
 }
