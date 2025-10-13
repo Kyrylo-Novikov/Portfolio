af79c379b2b237c3482e1eae2496a53746f2367f
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-10-13 05:29:11 +0200

    feat(cursor): add shadow that follows the cursor

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
@@ -24,4 +24,12 @@ import { FooterComponent } from '../shared/footer/footer.component';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {}
+export class LandingComponent {
+  @ViewChild('shadow', { static: true }) shadowEle!: ElementRef<HTMLDivElement>;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.shadowEle.nativeElement.style.top = event.clientY + `px`;
+    this.shadowEle.nativeElement.style.left = event.clientX + `px`;
+  }
+}
