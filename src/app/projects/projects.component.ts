d0d5c0176a28615c34792bab20d9e1f91adc2394
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-10-28 13:40:25 +0100

    refactor(projects): implement project display and finalize design

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+type Project = { name: string; usedTechs: string; imgPath: string };
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
+  hoverdIndex: number | null = null;
+  projectsArray: Project[] = [
+    {
+      name: 'Join',
+      usedTechs: 'Angular|TypeScript |HTML|CSS|Firebase',
+      imgPath: '/assets/imgs/projects/join.svg',
+    },
+    {
+      name: 'El Pollo Loco',
+      usedTechs: 'HTML|CSS|JavaScript',
+      imgPath: '/assets/imgs/projects/el-pollo-loco.svg',
+    },
+    {
+      name: 'Pokemon',
+      usedTechs: 'HTML|CSS|JavaScript',
+      imgPath: '/assets/imgs/projects/pokemon-searched.png',
+    },
+  ];
 }
