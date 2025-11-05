7abd152bc91c63ceaaed6126afcbec62762d0448
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-11-05 07:00:29 +0100

    feat(enum):centralize technology data using an enum

---
+++ src/app/models/tech-name.enum.ts
@@ -0,0 +1,35 @@
+export enum TechName {
+  Angular = 'Angular',
+  HTML = 'HTML',
+  CSS = 'CSS',
+  TypeScript = 'TypeScript',
+  JavaScript = 'JavaScript',
+  Firebase = 'Firebase',
+}
+
+export const Tech_DATA = {
+  [TechName.Angular]: {
+    name: 'Angular',
+    iconPath: 'assets/imgs/tech-icon/overlay/Angular.svg',
+  },
+  [TechName.HTML]: {
+    name: 'HTML',
+    iconPath: 'assets/imgs/tech-icon/overlay/HTML.svg',
+  },
+  [TechName.CSS]: {
+    name: 'CSS',
+    iconPath: 'assets/imgs/tech-icon/overlay/CSS.svg',
+  },
+  [TechName.TypeScript]: {
+    name: 'TypeScript',
+    iconPath: 'assets/imgs/tech-icon/overlay/TypeScript.svg',
+  },
+  [TechName.JavaScript]: {
+    name: 'JavaScript',
+    iconPath: 'assets/imgs/tech-icon/overlay/JavaScript.svg',
+  },
+  [TechName.Firebase]: {
+    name: 'Firebase',
+    iconPath: 'assets/imgs/tech-icon/overlay/Firebase.svg',
+  },
+};
