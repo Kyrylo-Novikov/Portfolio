01220caa37cc3c9b1148d6b3b05c33e24733dc9a
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-11-05 06:51:03 +0100

    feat(project):add PojectOverlay component

---
+++ src/app/projects/project-overlay/project-overlay.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, computed, inject } from '@angular/core';
+import { OverlayService } from '../../services/overlay.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-project-overlay',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './project-overlay.component.html',
+  styleUrl: './project-overlay.component.scss',
+})
+export class ProjectOverlayComponent {
+  private overlay = inject(OverlayService);
+  project = computed(() => this.overlay.activeProject());
+
+  close() {
+    this.overlay.close();
+  }
+}
