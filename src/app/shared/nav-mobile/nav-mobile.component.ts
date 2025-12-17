1c79dc167d2b138224cd2dde4cbd16de60a07d45
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-17 14:51:38 +0100

    feat(language-switch):add functionality to desktop and mobile navbars

--- src/app/shared/nav-mobile/nav-mobile.component.ts
+++ src/app/shared/nav-mobile/nav-mobile.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from './../../services/translate/translation.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-mobile',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './nav-mobile.component.html',
   styleUrl: './nav-mobile.component.scss',
 })
 export class NavMobileComponent {
   aktivSection: string = '';
   isOpen: boolean = false;
+
+  constructor(private translateService: TranslationService) {
+    // translateService.initLanguage();
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.switchLanguage(lang);
+  }
 
   setAktiv(section: string) {
     this.aktivSection = section;
@@ -19,5 +29,10 @@ export class NavMobileComponent {
 
   toggelNav() {
     this.isOpen = !this.isOpen;
+  }
+
+  isCurrentLang(lang: string): boolean {
+    const isMatch = this.translateService.isCurrentLang(lang);
+    return isMatch;
   }
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from './../../services/translate/translation.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(private translateService: TranslationService) {
+    // translateService.initLanguage();
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.switchLanguage(lang);
+  }
+
   aktivSection: string = '';
 
   setAktiv(section: string) {
     this.aktivSection = section;
+  }
+
+  isCurrentLang(lang: string): boolean {
+    const isMatch = this.translateService.isCurrentLang(lang);
+    return isMatch;
   }
 }
