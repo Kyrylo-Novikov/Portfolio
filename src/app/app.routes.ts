a64ba91fd649adcf7086578bd190250c316dd6d5
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-24 09:39:03 +0100

    feat(routes): integrate privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PolicyComponent } from './policy/policy.component';
 
 export const routes: Routes = [
   { path: ``, component: LandingComponent },
   { path: `legal-notice`, component: LegalNoticeComponent },
+  { path: `policy`, component: PolicyComponent },
 ];
