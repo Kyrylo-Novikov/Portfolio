7d3da08d8e5536530937f233fb0e65313340bcc0
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-24 09:37:07 +0100

    feat(policy): add privacy policy

---
+++ src/app/policy/policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-policy',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './policy.component.html',
+  styleUrl: './policy.component.scss',
+})
+export class PolicyComponent {}
