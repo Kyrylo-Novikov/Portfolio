adb79b15b52b3beb8fa12727b1ccb23a5ecf7bcf
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-28 14:54:28 +0100

    feat(letter-name): add router module

--- src/app/shared/letter-name/letter-name.component.ts
+++ src/app/shared/letter-name/letter-name.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-letter-name',
