085a1eee5537d9771a4c82a94f3e11ec7245041c
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-29 06:47:28 +0100

    feat(i18n): enable i18n in footer component with policy link

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LetterNameComponent } from '../letter-name/letter-name.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LetterNameComponent, RouterModule],
+  imports: [LetterNameComponent, RouterModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
