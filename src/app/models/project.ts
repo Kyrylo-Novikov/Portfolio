77d74b131b88275bd3c1060d3743e46fd7379f55
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-01-11 07:59:02 +0100

    docs(project):add documentation

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,9 +1,13 @@
 import { TechName } from './tech-name.enum';
 
+/**
+ * Defines the structure of the Projects.
+ */
 export interface Project {
   id: number;
   name: string;
   description: string;
+  /** Technologies used in the project,must be one of TechName enum */
   usedTechs: TechName[];
   imgPath: string;
   gitLink: string;
