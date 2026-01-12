357c044d59438955c277ddb7c26b464a906ea9ed
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-01-12 06:07:15 +0100

    docs(references): add documentation - rename hovered state for clarity

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -15,6 +15,7 @@ import {
 import { timeout } from 'rxjs';
 import { ScreenService } from './/../services/screen/screen.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { Reference } from '../models/reference';
 
 @Component({
   selector: 'app-references',
@@ -23,6 +24,16 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
 
+  /**
+   * Slide animation for horizontal movement
+   *
+   * Moves the element left or right depending on states
+   * Uses a configurable distance via animation parameters
+   *
+   * States:
+   * - 'left'  → slides element to the left
+   * - 'right' → slides element to the right
+   */
   animations: [
     trigger('slide', [
       state('left', style({ transform: 'translateX(-{{animationDist}})' }), {
@@ -36,28 +47,47 @@ import { TranslateModule } from '@ngx-translate/core';
   ],
 })
 export class ReferencesComponent {
+  /**
+   * Menu animation trigger state
+   * Default : null (no animation on init)
+   */
   protected menuState: 'left' | 'right' | null = null;
+
   currentIndex: number = 0;
-  activIndex = signal(0);
-  isHoveredLeft: boolean = false;
-  isHoveredRight: boolean = false;
+  /**
+   * Controls whether the image source is changed.
+   */
+  leftArrowIsHovered: boolean = false;
+  rightArrowIsHovered: boolean = false;
+
+  /**
+   * Default distance the element moves during the slide animation.
+   */
   animationDist: string = '32%';
 
+  /**
+   * Changes animationDist depending on the width of the display.
+   * For smooth animation on every screen width.
+   */
   constructor(private screen: ScreenService) {
     effect(() => {
-      if (screen.windowW() >= 1024) {
+      const width = this.screen.windowW();
+      if (width >= 1024) {
         this.animationDist = '32%';
-      } else if (screen.windowW() < 1024 && screen.windowW() > 800) {
+      } else if (width < 1024 && width > 800) {
         this.animationDist = '24%';
-      } else if (screen.windowW() < 800 && screen.windowW() > 500) {
+      } else if (width < 800 && width > 500) {
         this.animationDist = '18%';
-      } else if (screen.windowW() < 500) {
+      } else if (width < 500) {
         this.animationDist = '16.8%';
       }
     });
   }
 
-  referenceArray = signal<{ name: string; text: string }[]>([
+  /**
+   * Reactive list of references using i18n keys.
+   */
+  referenceArray = signal<Reference[]>([
     {
       name: 'REFERENZES.REF_1.NAME',
       text: 'REFERENZES.REF_1.TEXT',
@@ -72,11 +102,20 @@ export class ReferencesComponent {
     },
   ]);
 
+  /**
+   * Doubles the references to ensure enough items for a smooth slide animation.
+   */
   doubleRefArr = computed(() => [
     ...this.referenceArray(),
     ...this.referenceArray(),
   ]);
 
+  /**
+   * Starts the slide animation and updates the active index.
+   * @param change Direction of the slide
+   * - negative value: slide to the previous item 'right'
+   * - positive value: slide to the next item 'left'.
+   */
   slide(change: number) {
     this.menuState = change < 0 ? 'right' : 'left';
     setTimeout(() => {
@@ -85,6 +124,12 @@ export class ReferencesComponent {
     this.currentIndex = (this.currentIndex + change + 3) % 3;
   }
 
+  /**
+   * Update the reference array for the slideing animation.
+   * Moves the references to create continuos sliding effect.
+   * Resets the menuState on default after rearranging.
+   * @param change Slide direction, passed from `slide()` function
+   */
   arrayUpdate(change: number) {
     this.referenceArray.update((arr) =>
       change < 0
