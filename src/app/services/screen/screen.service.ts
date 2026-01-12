a2db782fb608aa15bcd1059d5048fd56ea8f0c87
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-01-12 05:56:15 +0100

    docs(screen):add documentation

--- src/app/services/screen/screen.service.ts
+++ src/app/services/screen/screen.service.ts
@@ -4,12 +4,22 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class ScreenService {
+  /**
+   * Holds the current window width
+   */
   width = signal(window.innerWidth);
 
   constructor() {
+    /**
+     * Updates the `width` signal whenever the window is resized
+     */
     window.addEventListener('resize', () => this.width.set(window.innerWidth));
   }
 
+  /**
+   * Indicates if the current screen width is considered mobile.
+   * @returns `true` if the window width is 800px or less, otherwise `false`.
+   */
   get isMobile() {
     return this.width() <= 800;
   }
