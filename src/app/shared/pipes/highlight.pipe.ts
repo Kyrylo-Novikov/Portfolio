3216c8d343f2dbd578129b07b31587a90898912c
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-01-15 12:11:03 +0100

    refactor(highlight): move the constructin of the regex to a independent function and add documentation

--- src/app/shared/pipes/highlight.pipe.ts
+++ src/app/shared/pipes/highlight.pipe.ts
@@ -1,6 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-
+/**
+ * All matching words are wrapped in a <span> elemetn with the CSS class "highlight-legal-words" to giv custom styling.
+ *
+ * Since the pipe generates HTML markup dynamically, the result is
+ * sanitized using Angular's DomSanitizer and returned as SafeHtml.
+ *
+ * exaple:
+ * {{text | highlight : legalWords}}
+ */
 @Pipe({
   name: 'highlight',
   standalone: true,
@@ -8,16 +16,36 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class HighlightPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(value: string | null | undefined, words: string[]): SafeHtml {
-    if (!value) return '';
-    if (!words || words.length === 0) return value;
-
+  /**
+   * Highlights all occurences of the provided keywords within the given text.
+   *
+   * @param text Text that have to be processed
+   * @param words Array of words that should be highlighted
+   * @returns The processed text with highlighted keywords as SafeHtml,
+   *          or the original text if no highlighting is applied
+   */
+  transform(text: string, words: string[]): SafeHtml | string {
+    if (!text) return '';
+    if (!words || words.length === 0) return text;
     const highlightWord = words.join('|');
-    const regex = new RegExp(`(${highlightWord})`, 'gi');
-
-    const replaced = value.replace(regex, (match: string) => {
-      return `<span class="highlight-legal-words"> ${match}</span>`;
-    });
+    const replaced = text.replace(
+      this.defineRegex(highlightWord),
+      (match: string) => {
+        return `<span class="highlight-legal-words"> ${match}</span>`;
+      }
+    );
     return this.sanitizer.bypassSecurityTrustHtml(replaced);
+  }
+
+  /**
+   * Creats a case-insensitive, global regular expression
+   * to match all providet keywords inside the text.
+   *
+   * @param serched Pattern
+   * @returns The Configurated regex
+   */
+  defineRegex(serched: string) {
+    const regex = new RegExp(`(${serched})`, 'gi');
+    return regex;
   }
 }
