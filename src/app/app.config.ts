62b6465cf05751281711a8d9a8b1903531dbcb18
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-01-18 13:09:11 +0100

    docs(config):add documentation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,10 +7,30 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
+/**
+ * Loader factory für ngx-translate
+ * load translation files from ./assets/i18n/
+ *
+ * @param http - Anfular HttpClient
+ * @returns TranslateHttpLoader Instanz
+ */
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
+/**
+ * Main configuration of the applicatin
+ *
+ * Configures:
+ * - HTTP client for API request
+ * - Internalisation (i18n) with ngx-translate
+ *    - Default language: English
+ *    - Translation files: ./assets/i18n/
+ * - Routing with scroll behavior
+ *    - Anchor scrolling enabled for fragments
+ *    - Scroll position restort on navigation
+ * -Browser animations
+ */
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
